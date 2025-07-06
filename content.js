e2617ab1c08e20f952de2dfe151f3ad6ecaa22b2
Siddy096 <varhadisiddhesh8@gmail.com>
2025-07-06 20:43:11 +0530

    file opening issue fix

--- content.js
+++ content.js
@@ -1,12 +1,18 @@
 const domains = window.location.hostname;
+const pathname = window.location.pathname;
 
-fetch(chrome.runtime.getURL('updated_format_blocked.txt'))
-    .then(response => response.text())
-    .then(fileContent => {
-        if(fileContent.includes(domains)){
-            window.location.href = chrome.runtime.getURL("block.html");
-        }
-    });
+const fileExtensions = ['.pdf', '.jpg', '.jpeg', '.png', '.doc', '.mp4', '.txt', '.csv']
+const isFile = fileExtensions.some(ext => pathname.toLowerCase().endsWith(ext));
+
+if(!isFile){
+    fetch(chrome.runtime.getURL('updated_format_blocked.txt'))
+        .then(response => response.text())
+        .then(fileContent => {
+            if(fileContent.includes(domains)){
+                window.location.href = chrome.runtime.getURL("block.html");
+            }
+        });
+}
 // const fs = require('fs');
 // const fileContent = fs.readFileSync('updated_format_blocked.txt', 'utf8');
 // const siteToSearch = domains;
